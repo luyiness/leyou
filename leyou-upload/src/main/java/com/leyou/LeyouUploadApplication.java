177c2d61c718e719caa41a98cdc26b6cd98f580e
luyi <luyiness@gmail.com>
2021-03-14 16:06:35 +0800

    5.图片上传（有点问题：无法上传到服务器）

---
+++ leyou-upload/src/main/java/com/leyou/LeyouUploadApplication.java
@@ -0,0 +1,17 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Date:2021/03_2:38 下午
+ * @Description：
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LeyouUploadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouUploadApplication.class, args);
+    }
+}
