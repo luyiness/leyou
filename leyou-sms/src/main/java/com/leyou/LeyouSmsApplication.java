2eaa3cfff7b7f11271110e43b192170ee2e127ff
luyi <luyiness@gmail.com>
2021-03-20 07:52:01 +0800

    21.Lsms初始

---
+++ leyou-sms/src/main/java/com/leyou/LeyouSmsApplication.java
@@ -0,0 +1,15 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Date:2021/03_8:27 下午
+ * @Description：
+ */
+@SpringBootApplication
+public class LeyouSmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouSmsApplication.class, args);
+    }
+}
