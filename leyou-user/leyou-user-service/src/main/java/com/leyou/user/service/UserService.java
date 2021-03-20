08532365909707770c3a36544f63fedbe75a18d8
luyi <luyiness@gmail.com>
2021-03-20 08:35:13 +0800

    22.M完成短信注册

--- leyou-user/leyou-user-service/src/main/java/com/leyou/user/service/UserService.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/service/UserService.java
@@ -1,9 +1,16 @@
 package com.leyou.user.service;
 
+import com.leyou.common.utils.NumberUtils;
 import com.leyou.user.mapper.UserMapper;
 import com.leyou.user.pojo.User;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Date:2021/03_2:51 下午
@@ -14,6 +21,14 @@ public class UserService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    static final String KEY_PREFIX = "USER:VERIFY";
 
     /**
      * 手机号、用户名的唯一性校验
@@ -31,5 +46,24 @@ public class UserService {
             return null;
         }
         return this.userMapper.selectCount(user) == 0;
+    }
+
+    public void sendVerifyCode(String phone) {
+        if (StringUtils.isBlank(phone)) {     //是commons.lang3依赖中的方法
+            return;
+        }
+
+        //1、生成验证码
+        String code = NumberUtils.generateCode(6);  //调用commons模块中工具类的方法
+
+        //2、发送消息到radditMQ
+        HashMap<String,String> map = new HashMap<>();
+        map.put("phone", phone);
+        map.put("code", code);
+
+        this.amqpTemplate.convertAndSend("leyou.sms.exchange","sms.verify.code",map);
+
+        //3、将验证码保存到redis中
+        this.redisTemplate.opsForValue().set(KEY_PREFIX + phone, code,1, TimeUnit.MINUTES);
     }
 }
